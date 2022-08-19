970f6c3fa50d007725060d0c4484e4721d980d1a
Eric Lam <ericlam@vitessedata.com>
2022-08-19 18:12:28 +0100

    revert

--- contrib/kite_fdw/kite_fdw.c
+++ contrib/kite_fdw/kite_fdw.c
@@ -338,9 +338,6 @@ kite_fdw_handler(PG_FUNCTION_ARGS)
 {
 	FdwRoutine *routine = makeNode(FdwRoutine);
 
-	__int128_t d128;
-	char *p;
-
 	/* Functions for scanning foreign tables */
 	routine->GetForeignRelSize = postgresGetForeignRelSize;
 	routine->GetForeignPaths = postgresGetForeignPaths;
@@ -407,10 +404,6 @@ kite_fdw_handler(PG_FUNCTION_ARGS)
 	routine->ForeignAsyncConfigureWait = postgresForeignAsyncConfigureWait;
 	routine->ForeignAsyncNotify = postgresForeignAsyncNotify;
 #endif
-	d128 = 123;
-	p = dec128_to_string(d128, 2);
-	elog(LOG, "decimal = %s", p);
-	if (p) free(p);
 
 	PG_RETURN_POINTER(routine);
 }
